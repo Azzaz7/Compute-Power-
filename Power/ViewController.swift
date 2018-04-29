1c979f7af1268e4480e806cafb6943857f07ebd7
azzaz <azzaz@azzazs-Mac.local>
2018-04-30 00:29:42 +0200

    First Uploading

--- Power/ViewController.swift
+++ Power/ViewController.swift
@@ -10,6 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldEnterNumber: UITextField!
+    @IBOutlet weak var textFieldEnterPower: UITextField!
+    
+    
+    @IBOutlet weak var label2ShoeRes: UILabel!
+    
+    
+    @IBAction func computePower(_ sender: Any) {
+        
+        let num = NSCharacterSet.decimalDigits.inverted
+        
+        if !(textFieldEnterPower.text?.isEmpty)! &&
+            !(textFieldEnterPower.text?.isEmpty)! &&
+            (textFieldEnterPower.text?.rangeOfCharacter(from: num) == nil ) &&
+            (textFieldEnterNumber.text?.rangeOfCharacter(from: num) == nil)
+            
+            
+        {
+            var   res :Double = 1.0000
+            
+            let base  = (Double)(textFieldEnterNumber.text!)
+            var power = (Double)(textFieldEnterPower.text!)
+            
+            repeat
+            {
+                res *= base!
+                power = power!-1
+            }
+                while power! > 0.0
+            
+            
+            
+            label2ShoeRes.text = (String)(res)
+            res = 1
+        }
+            
+        else
+        {
+            label2ShoeRes.text = "plase enter the 2 text field"
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
